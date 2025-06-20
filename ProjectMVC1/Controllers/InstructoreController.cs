eef12e55705ef93468f343526040b3bd8dd5872a
mohamed ahmad <amohamedahmad68@gmail.com>
2025-06-20 16:58:44 +0300

    day 3

--- ProjectMVC1/Controllers/InstructoreController.cs
+++ ProjectMVC1/Controllers/InstructoreController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ProjectMVC1.Models;
 
 namespace ProjectMVC1.Controllers
@@ -40,5 +41,89 @@ namespace ProjectMVC1.Controllers
 
             return View(instructor);
         }
+
+        public IActionResult Edit(int id) {
+            var instructor = _context.Instructores.FirstOrDefault(i => i.InstructoreId == id);
+            if (instructor == null)
+                return NotFound();
+
+            InstrctrWithDprtmntViewModel viewModel = new()
+            {
+                InstructoreId = instructor.InstructoreId,
+                Name = instructor.Name,
+                Salary = instructor.Salary,
+                Address = instructor.Address,
+                DepartmentId = instructor.DepartmentId,
+                CourseId = instructor.CourseId,
+                Image = instructor.Image,
+                Departments = _context.Departments.ToList(),
+                Courses = _context.Courses.ToList(),
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult SavaChanges(int id, InstrctrWithDprtmntViewModel updatedInstructore)
+        {
+            if (id != updatedInstructore.InstructoreId)
+            {
+                return BadRequest();
+            }
+
+            if(string.IsNullOrWhiteSpace(updatedInstructore.Name))
+            {
+                updatedInstructore.Departments = _context.Departments.ToList();
+                updatedInstructore.Courses = _context.Courses.ToList();
+                return View("Edit", updatedInstructore);
+            }
+
+            var orginal = _context.Instructores.FirstOrDefault(i => i.InstructoreId == id);
+            if (orginal == null)
+                return NotFound();
+
+            orginal.Name = updatedInstructore.Name;
+            orginal.Salary = updatedInstructore.Salary;
+            orginal.Address = updatedInstructore.Address;
+            orginal.Image = updatedInstructore.Image;
+            orginal.DepartmentId = updatedInstructore.DepartmentId;
+            orginal.CourseId = updatedInstructore.CourseId;
+
+            _context.Attach(orginal);
+            _context.Entry(orginal).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult New()
+        {
+            InstrctrWithDprtmntViewModel instructoreViewModel = new InstrctrWithDprtmntViewModel();
+            instructoreViewModel.Departments = _context.Departments.ToList();
+            instructoreViewModel.Courses = _context.Courses.ToList();
+            return View(instructoreViewModel);
+        }
+
+        public IActionResult NewInstructor(InstrctrWithDprtmntViewModel NewInstructore) {
+            if (NewInstructore.Name == null) {
+                NewInstructore.Departments = _context.Departments.ToList();
+                NewInstructore.Courses = _context.Courses.ToList();
+                return View("New", NewInstructore);
+            }
+
+            Instructore instructor = new Instructore
+            {
+                Name = NewInstructore.Name,
+                Address = NewInstructore.Address,
+                Image = NewInstructore.Image,
+                Salary = NewInstructore.Salary,
+                DepartmentId = NewInstructore.DepartmentId,
+                CourseId = NewInstructore.CourseId
+            };
+
+            _context.Instructores.Add(instructor);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
+
 }
---
+++ ProjectMVC1/Models/InstrctrWithDprtmntViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectMVC1.Models
+{
+    public class InstrctrWithDprtmntViewModel
+    {
+        public int InstructoreId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Address { get; set; }
+        public double Salary { get; set; }
+        public int DepartmentId { get; set; }
+        public int CourseId { get; set; }
+        public List<Department> Departments { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
